2f06e2b62446302f27079c7732667955a84a68e6
Monir Hossain Abde Rabby <96588176+monirhabderabby@users.noreply.github.com>
2022-05-22 23:50:58 +0600

    make api for get all user and get products detaisl

--- index.js
+++ index.js
@@ -14,20 +14,49 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@aorus.lxgn5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+//Verification JSON WEB Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: "unAuthorized access" });
+    }
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET_KEY, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ mesage: "Forbidden Access" });
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
+
 async function run(){
     try{
         await client.connect();
         const userCollection = client.db("AORUS_WORLD").collection("users");
+        const productCollection = client.db("AORUS_WORLD").collection("products");
         console.log("DB Connected");
 
         //ALL GET API
+
+        //get all users information with this API
+        app.get('/users',verifyJWT, async(req, res)=> {
+            const result = await userCollection.find().toArray();
+            res.send(result)
+        })
+
+        //get all product data
+        app.get("/parts", verifyJWT, async (req, res)=> {
+            const result = await productCollection.find({}).toArray();
+            res.send(result)
+        })
 
 
         //ALL POST API
         
 
         //ALL PUT API
-        app.put('/user', async (req, res)=> {
+        app.put('/user',  async (req, res)=> {
             const user = req.body;
             const filter = {email: user.email};
             const options = {upsert: true};
